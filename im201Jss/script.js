a9b81eb16778456a07840c895656465343e4ecef
functionallypoised <badartistpresent@gmail.com>
2026-05-19 11:51:09 +1000

    neaten and understand

--- im201Jss/script.js
+++ im201Jss/script.js
@@ -1,55 +1,42 @@
-//first get access to the audito element so we can control it from here
-
+// connect html element to id tag,
+//  to asks connection to load source {the media file},
+//  for js to select as a variable
+// to control from button
 const airportAudio = document.querySelector("#airport-audio");
-console.log(airportAudio);
-
-// let us access video too
 const starVideo = document.querySelector("#star-video");
-console.log(starVideo);
-
-//similarly access the play button
 const playButton = document.querySelector("#play-button");
-console.log(playButton);
+const pauseButton = document.querySelector("#pause-button");
+const popAudio = document.querySelector("#pop-audio");
+const popButton = document.querySelector("#pop-button");
+const msg = document.querySelector("#msg");
 
+// [command]Button.addEventListener("click", [command]);
+// is the the control/connect of the button/function 
+// in order for the User to control the media content function
+// within browser interface: each arranged command listens for event/click
+// to then connect each respective function.
 playButton.addEventListener("click", playAudio);
+pauseButton.addEventListener("click", pauseAudio);
+popButton.addEventListener("click", makeItPop);
+
+// each 
+// function [command]Audio (); 
+// [command].play(); 
+// msg.textContent = "text for User"
+// below is aforementioned function,
+//  alongside textContent to inform User on interface
 
 function playAudio() {
   airportAudio.play();
   msg.textContent = "airport is playing";
 }
 
-//similarly access the pause button
-const pauseButton = document.querySelector("#pause-button");
-console.log(pauseButton);
-
-pauseButton.addEventListener("click", pauseAudio);
-
 function pauseAudio() {
-  airportAudio.play();
+  airportAudio.pause();
   msg.textContent = "airport is paused";
 }
-
-//similarly access the pop button
-const popAudio = document.querySelector("#pop-audio");
-console.log(popAudio);
-const popButton = document.querySelector("#pop-button");
-console.log(popButton);
-
-popButton.addEventListener("click", makeItPop);
 
 function makeItPop() {
   popAudio.play();
   msg.textContent = "popping...";
 }
-
-// const body = DocumentFragment.querySelector("body");
-// body.addEventListener("keydown", playVideo);
-
-// function playVideo() {
-//   if (event.code === "Space") {
-//     // Prevent default behaviour (like scrolling down page)
-//     event.preventDefault();
-//     console.log("Spacebar was pressed!");
-//     //starVideo.play();
-//   }
-// }
